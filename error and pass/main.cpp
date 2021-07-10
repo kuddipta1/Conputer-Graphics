cdcad502410e2c35b7035ecefb2c42a59e7d5c77
UDDIPTA <78270763+kuddipta1@users.noreply.github.com>
2021-07-11 02:44:16 +0600

    Add files via upload

---
+++ error and pass/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include<GL/gl.h>
+#include <GL/glut.h>
+
+void myDisplay(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3ub (128, 128, 128);
+glPointSize(5.0);
+glBegin(GL_LINES);
+glVertex2i(0,240);
+glVertex2i(640,240);
+glVertex2i(320,480);
+glVertex2i(320,0);
+glEnd();
+glFlush ();
+}
+
+void myInit (void)
+{
+glClearColor(0.0, 0.0, 0.0, 0.0);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+
+
+
+
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (640, 480);
+glutInitWindowPosition (100, 150);
+glutCreateWindow ("");
+glutDisplayFunc(myDisplay);
+myInit ();
+glutMainLoop();
+}
